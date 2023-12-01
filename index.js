29554869b0a2877e318dd9f98539db772e7aae94
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-12-01 11:38:30 +0100

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+// `github:boyum/prettier-config` sets `arrowParens` to `avoid`.
+// Prettier 4.0.0-alpha.3 does not respect this setting,
+// but instead uses the default `always`
+const func = a => {};
